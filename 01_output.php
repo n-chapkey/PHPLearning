745c2a56fb80af4a9ff5c8219403d6c496dd2dd5
Nicholas Chapkey <nchapkey@gmail.com>
2023-03-18 13:01:18 -0400

    Renamed index -- added two files related to variables and array -- learned output in php

---
+++ 01_output.php
@@ -0,0 +1,30 @@
+<?php
+//echo
+// echo 123, 'Hello', 10.5;
+
+//print
+// print 123;
+
+//print_r
+// print_r ([1,2,3]);
+
+//var_dump()
+// var_dump("hello");
+
+// var_export()
+// var_export("hello")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= 'Post One';?></h1>
+</body>
+</html>
---
+++ 02_variables.php
@@ -0,0 +1,20 @@
+<?php 
+
+$name = 'Brad'; //String
+$age = 40; //Int
+$has_kids = true;
+
+// echo $name;
+// echo $age;
+// var_dump($has_kids);
+
+// echo $name . ' is ' . $age . ' years old';
+
+// echo "${name} is ${age} years old";
+
+$x = '5' + '5';
+
+
+define('HOST', 'localhost');
+define('DB_NAME' , 'dev_db');
+?>
